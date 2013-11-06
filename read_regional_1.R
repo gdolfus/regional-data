2954ee7118e93ece5cb60647e8899e9518837a60
gdolfus <gdolfus@gmail.com>
2013-11-06 11:37:34 +0200

    read all industries and minor changes

--- read_regional_1.R
+++ read_regional_1.R
@@ -29,40 +29,40 @@ years <- as.character(1985:1995)
 places <- read.table(paste(dirname.data, "seutukunnat.txt", 
 	sep = ""))
 places <- places[, 1]
-indust <- read.csv(paste(dirname.data, "toimialat.csv", sep = ""))
-indust <- c("maa", "metsa", "kala", "elintar", "puu", 
-	"perus", "muu_teoll", "raken", "tukku", "majoitus", 
+indust <- read.csv(paste(dirname.data, "toimialat.csv", 
+	sep = ""))
+indust <- c("all","maa", "metsa", "kala", "elintar", "puu", 
+	"metal", "muu_teoll", "raken", "tukku", "majoitus", 
 	"kuljetus", "rahoitus", "kiinteisto", "asuntojen", 
 	"hallinto", "koulutus", "terveys", "muut_palv", 
-	"kotipal", "erittelematon")
+	"kotipalv", "erittelematon")
 
 vars.original <- c("Tuotos perushintaan", "Valituotekaytto ostajanhintaan", 
- "Arvonlisays, brutto perushintaan", "Palkansaajakorvaukset", 
- "Tyolliset", "Yrittajat", "Palkansaajat", "Tehdyt tyotunnit", 
- "Yrittajien tehdyt tyotunnit", "Palkansaajien tehdyt tyotunnit", 
- "Kiintean paaoman bruttomuodostus")
+	"Arvonlisays, brutto perushintaan", "Palkansaajakorvaukset", 
+	"Tyolliset", "Yrittajat", "Palkansaajat", "Tehdyt tyotunnit", 
+	"Yrittajien tehdyt tyotunnit", "Palkansaajien tehdyt tyotunnit", 
+	"Kiintean paaoman bruttomuodostus")
 vars <- c("output", "input", "value_added", "wage", 
-	"empl_tot", "empl_entre", "empl_workers", "hrs", "hrs_entre", "hrs_empl", 
-	"fixed_capital")
+	"empl_tot", "empl_entre", "empl_workers", "hrs", 
+	"hrs_entre", "hrs_empl", "fixed_capital")
 
 files <- NULL
 obs <- matrix(NA, nrow = length(places), ncol = length(years) * 
 	length(indust))
 jcounter <- 0
 
-#for (i in indust) {
-for (i in c("all", "maa")) {
+for (i in indust) {
+# for (i in c("all", "maa")) {
 	industplace <- paste(i, "-", as.character(places), 
 		sep = "")
 	for (j in years) {
 		jcounter <- jcounter + 1
 		for (k in places) {
 			tmp <- paste(industplace, "-", j, sep = "")
-			obs[, jcounter] <- tmp	
+			obs[, jcounter] <- tmp
 		}
 		tmp <- paste(i, "-", j, ".csv", sep = "")
-			print(tmp)
-			files <- rbind(files, tmp)
+		files <- rbind(files, tmp)
 	}
 }
 
@@ -78,48 +78,54 @@ start <- "yes"
 counter <- 0
 
 for (i in files) {
-	dat <- read.csv(paste(dirname.data, i, sep = ""), header = F)
+	dat <- read.csv(paste(dirname.data, i, sep = ""), 
+		header = F)
 	tmp <- dat[-c(1, 2, 3, 4, 5, 6, 7, 87:96), -c(1, 
 		2, 3, 4)]
 	counter <- counter + 1
 	rownames(tmp) <- obs[, counter]
 
 	if (start == "yes") {
-		tmpall <- rep(tmp)
+		tmp.all <- rep(tmp)
 		start <- "no"
 	}
 
-	tmpall <- rbind(tmpall, tmp)
+	tmp.all <- rbind(tmp.all, tmp)
 }
 
 rm(i, dat, tmp, start)
 
 # Remove the double entry.
-tmpall <- tmpall[-c(1:length(places)), ]
+tmp.all <- tmp.all[-c(1:length(places)), ]
 
 # Add the column names.
-colnames(tmpall) <- vars
+colnames(tmp.all) <- vars
 
 # Convert factors to numerical.
-tmpall$output <- as.numeric(as.character(tmpall$output))
-tmpall$input <- as.numeric(as.character(tmpall$input))
-tmpall$value_added <- as.numeric(as.character(tmpall$value_added))
-tmpall$wage <- as.numeric(as.character(tmpall$wage))
-tmpall$empl_tot <- as.numeric(as.character(tmpall$empl_tot))
-tmpall$entre <- as.numeric(as.character(tmpall$empl_entre))
-tmpall$empl <- as.numeric(as.character(tmpall$empl_workers))
-tmpall$hrs <- as.numeric(as.character(tmpall$hrs))
-tmpall$hrs_entre <- as.numeric(as.character(tmpall$hrs_entre))
-tmpall$hrs_empl <- as.numeric(as.character(tmpall$hrs_empl))
-tmpall$fixed_capital <- as.numeric(as.character(tmpall$fixed_capital))
+tmp.all$output <- as.numeric(as.character(tmp.all$output))
+tmp.all$input <- as.numeric(as.character(tmp.all$input))
+tmp.all$value_added <- as.numeric(as.character(tmp.all$value_added))
+tmp.all$wage <- as.numeric(as.character(tmp.all$wage))
+tmp.all$empl_tot <- as.numeric(as.character(tmp.all$empl_tot))
+tmp.all$entre <- as.numeric(as.character(tmp.all$empl_entre))
+tmp.all$empl <- as.numeric(as.character(tmp.all$empl_workers))
+tmp.all$hrs <- as.numeric(as.character(tmp.all$hrs))
+tmp.all$hrs_entre <- as.numeric(as.character(tmp.all$hrs_entre))
+tmp.all$hrs_empl <- as.numeric(as.character(tmp.all$hrs_empl))
+tmp.all$fixed_capital <- as.numeric(as.character(tmp.all$fixed_capital))
 
 
 # Housekeeping.
-write.csv(tmpall, paste(dirname.data,"aaa-panel.csv",sep=''))
-write.csv(indust, paste(dirname.data,"aaa-indust.csv",sep=''))
-write.csv(as.character(places),paste(dirname.data,"aaa-places.csv",sep=''))
-write.csv(years,paste(dirname.data,"aaa-years.csv",sep=''))
-write.csv(cbind(vars.original,vars),paste(dirname.data,"aaa-vars.csv",sep=''))
+write.csv(tmp.all, paste(dirname.data, "aaa-panel.csv", 
+	sep = ""))
+write.csv(indust, paste(dirname.data, "aaa-indust.csv", 
+	sep = ""))
+write.csv(as.character(places), paste(dirname.data, 
+	"aaa-places.csv", sep = ""))
+write.csv(years, paste(dirname.data, "aaa-years.csv", 
+	sep = ""))
+write.csv(cbind(vars.original, vars), paste(dirname.data, 
+	"aaa-vars.csv", sep = ""))
 
 
 
